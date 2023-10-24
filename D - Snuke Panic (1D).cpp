02b96d4a09ed803fce82f732861884947ed75a54
Manan Chhajed <113279468+Manan-Chhajed@users.noreply.github.com>
2023-10-24 17:48:17 +0530

    Update D - Snuke Panic (1D).cpp

--- D - Snuke Panic (1D).cpp
+++ D - Snuke Panic (1D).cpp
@@ -1,14 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void cmc() {
-#ifndef ONLINE_JUDGE
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-    freopen("err.txt", "w", stderr);
-#endif
-}
-
 using ll = long long;
 
 /*
@@ -69,11 +61,7 @@ void solve()
 }
 
 int main() {
-    cmc();
     ios_base::sync_with_stdio(0); cin.tie(0);
-
     solve();
-
-    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << "\n";
     return 0;
 }
